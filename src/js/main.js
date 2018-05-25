5a7e63af23ea3dff84bb7cc002e601d0c4b1f3ee
Jason Snider <root@jasonsnider.com>
2018-05-25 17:06:12 -0500

    Added raandomize controlls and UI updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,78 @@
+//console.log(nasaKey);
+
 var apod = {
-    init: function() {
-        var nasaKey = 'xYpVHthqInxK0wzGAEYgSpkoeIwonVdbpQ59dizB';
-        var url = 'https://api.nasa.gov/planetary/apod?api_key=';
+  //Create a random apodDate
+  randomDate: function(start, end) {
+    var date = new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    );
 
-        $.ajax({
-            url: url + nasaKey
-        }).done(function(result){
+    //Format the date
+    var d = date.getDate();
+    var m = date.getMonth() + 1;
+    var y = date.getFullYear();
 
-            $('#apodTitle').text(result.title);
-
-            $('#apodImg')
-                .attr('src', result.url)
-                .attr('alt', result.title);
-
-            $('#apodCopyright').text('Copyright: '  + result.copyright);
-
-            $('#apodDate').text('Date: '  + result.date);
-
-            $('#apodDesc').text(result.explanation);
-
-        }).fail(function(result){
-            console.log(result);
-        });
+    if(m < 10){
+      m = '0' + m;
     }
+
+    if(d < 10){
+      d = '0' + d;
+    }
+
+    return y + '-' + m + '-' + d;
+
+  },
+
+  buildDOM: function(result){
+
+    $('#apodTitle').text(result.title);
+
+    if(result.media_type === 'video') {
+      $("#apodImg").hide();
+      $("#apodVideo > iframe").attr("src", result.url).show();
+    }else{
+      $("#apodVideo").hide();
+      $("#apodImg").attr("src", result.url).attr('alt', result.title).show();
+    }
+
+    if(result.copyright){
+        $('#apodCopyright').html('&copy;' + result.copyright + '&nbsp;');
+    }
+    
+    $('#apodDate').text(result.date);
+    $('#apodDesc').text(result.explanation);
+  },
+
+  getRequest: function(){
+    var _this = this;
+    //2011-03-28
+    var date = this.randomDate(new Date(1995, 5, 16), new Date());
+    //var date = this.randomDate(new Date(2011, 2, 28), new Date(2011, 2, 28));
+
+    var url = "https://api.nasa.gov/planetary/apod?api_key="
+      + 'xYpVHthqInxK0wzGAEYgSpkoeIwonVdbpQ59dizB'
+      + '&date=' + date;
+
+    $.ajax({
+      url:url
+    }).done(function(result){
+      _this.buildDOM(result);
+    }).fail(function(result){
+      console.log(result);
+    });
+  },
+  //Application constructor
+  init: function() {
+    this.getRequest();
+  }
 };
 
 apod.init();
+
+$(function(){
+  $('#btnRandom').on('click',function(e){
+    apod.getRequest();
+    e.preventDefault();
+  });
+});
